a73176d8d27085b67070dbf34b2a9aab1bcddd99
bmkuter <10656287+bkuter@users.noreply.github.com>
2018-02-07 23:04:29 -0500

    Adding URL images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+
 
 
 @NgModule({
